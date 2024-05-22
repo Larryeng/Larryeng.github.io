f5af22eb088cb21780206b6fc6dc166c041fb06c
Larryeng <aaaa0909726897@gmail.com>
2024-05-23 00:21:02 +0800

    Site updated: 2024-05-23 00:21:02

---
+++ js/title.js
@@ -0,0 +1,1 @@
+var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",(function(){document.hidden?(document.title="😑不要冷落我",clearTimeout(titleTime)):(document.title="🥳我又復活了",titleTime=setTimeout((function(){document.title=OriginTitile}),2e3))}));
