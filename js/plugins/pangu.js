03a2e1b790cff2d84ef43684b579ac33a8f909f5
SharkY.Y <aaaa0909726897@gmail.com>
2026-03-10 23:41:43 +0800

    Site updated: 2026-03-10 23:41:40

--- js/plugins/pangu.js
+++ js/plugins/pangu.js
@@ -1,1 +1,1 @@
-function initPanguJS(){pangu.spacingElementByClassName("markdown-body"),pangu.autoSpacingPage()}document.addEventListener("DOMContentLoaded",initPanguJS);try{swup.hooks.on("page:view",initPanguJS)}catch(n){}
+export default function initPangu(){"undefined"!=typeof pangu&&(pangu.spacingElementByClassName("markdown-body"),pangu.autoSpacingPage())}
